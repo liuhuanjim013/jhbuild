2a45e5a05bb7c311d44a9be224a20d06add9222b
Binbin Ye <binbin.ye@git.mujin.co.jp>
2017-06-28 17:29:41 +0900

    Need this to use qt5 qmake

--- jhbuild/modtypes/qmake.py
+++ jhbuild/modtypes/qmake.py
@@ -86,7 +86,12 @@ class QMakeModule(MakeModule, DownloadableModule):
 
     def do_install(self, buildscript):
         buildscript.set_action(_('Installing'), self)
-        self.make(buildscript, 'install')
+        # self.make(buildscript, 'install')
+        # qt5 version of qmake has only one way of controlling the install prefix for the modules, and that is through INSTALL_ROOT
+        destdir = self.prepare_installroot(buildscript)
+        makeargs = self.get_makeargs(self, buildscript) + ' INSTALL_ROOT="' + destdir + '"'
+        self.make(buildscript, 'install', makeargs=makeargs)
+
         self.process_install(buildscript, self.get_revision())
     do_install.depends = [PHASE_BUILD]
 
